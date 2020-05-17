76942ca05b258182fbcab6a5a4a28234bbb77d98
christanplaza <christan.shane.plaza@sun-asterisk.com>
2020-05-17 20:46:13 +0800

    Initial Commit

---
+++ Ubuntu_18_04/01_apt_get_install.sh
@@ -0,0 +1,22 @@
+# Install base tools
+
+# Set working directory as the directory of this script
+cd "$(dirname "$0")"
+
+# Updates & Install
+sudo apt-get update
+
+# Install Chromium
+sudo apt-get install -Y chromium-browser
+
+# ZSH
+sudo apt-get install -y zsh
+
+# Powerfile Fonts (For ZSH)
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts && ./install.sh && cd .. && rm -rf fonts
+
+# Oh-my-zsh
+rm -fr ~/.oh-my-zsh/
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
